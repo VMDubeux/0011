85615dec78db5ca76df0cb3a3c5a1fc361603895
Victor Dubeux <adv.victordubeux@gmail.com>
2023-02-03 20:09:44 -0300

    P11 C2 - Envio definitivo de projeto

---
+++ ProjetoOnze/ProjetoOnze/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Exercicio
+{
+    class ProjetoOnze
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua senha:");
+            int senha = int.Parse(Console.ReadLine());
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha Inválida. Tente novamente.");
+                senha = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Acesso Permitido.");
+        }
+    }
+}
